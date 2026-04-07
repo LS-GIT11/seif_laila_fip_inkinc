c41fa638118efe64e75d626f46a2ab4223ef85c7
Laila Seif <lailaseif710@gmail.com>
2026-04-07 16:57:58 -0400

    major setup frontend file structure
    i set up majority of the frontend... this is so when i am working i know i have pretty much all i need so far
    
    mainly setting up basic folders, adding my files, ensuring my sass is working by testing it with a background colour using my brand colours, and overall having all i need to start strong

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="frontend/css/main.css">
+    <link rel="stylesheet" href="frontend/css/grid.css">
+    <script type="module" src="frontend/js/main.js"></script>
+    <title>Homepage - INK INC.</title>
+</head>
+<body>
+
+    <header></header>
+
+    <main>
+        
+        <section class="grid-con"></section>
+
+        <section class="grid-con"></section>
+
+        <section class="grid-con"></section>
+
+        <section class="grid-con"></section>
+
+    </main>
+
+    <footer></footer>
+    
+</body>
+</html>
